73e520c9f6d6e562aad0316519a4437446ad67ef
SLM <sabrina.martorelli@gmail.com>
2023-03-29 21:05:08 +0100

    Install material icons

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,6 +1,32 @@
 
+
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+//import RestoreIcon from '@mui/icons-material/Restore';
+//import FavoriteIcon from '@mui/icons-material/Favorite';
+//import LocationOnIcon from '@mui/icons-material/LocationOn';
+
+
 const Footer = () => {
-    return ( <h1>Footer</h1> );
+    const [value, setValue] = React.useState(0);
+
+    return (
+      <Box sx={{ width: 500 }}>
+        <BottomNavigation
+          showLabels
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+        >
+          <BottomNavigationAction label="Lnk" icon={<RestoreIcon />} />
+          <BottomNavigationAction label="Github" icon={<FavoriteIcon />} />
+          <BottomNavigationAction label="Email" icon={<LocationOnIcon />} />
+        </BottomNavigation>
+      </Box>
+    );
 }
  
 export default Footer;
