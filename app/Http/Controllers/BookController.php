4e255a439bcf43c63ad25457ac4217614ac968c3
SLM <sabrina.martorelli@gmail.com>
2023-03-29 10:11:28 +0100

    Adds function on BookController (store)  to add new book

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Book;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class BookController extends Controller
 {
@@ -13,4 +14,21 @@ class BookController extends Controller
 
         return response()->json($books);
     }
+
+    public function store(Request $request) {
+
+        $book = new Book();
+
+        $book->title = $request->title;
+        $book->author = $request->author;
+        $book-> description = $request-> description;
+        $book-> published_date = Carbon::parse($request->'published_date'->toDateString();
+             
+        $book->save();
+
+    }
+
+
+
+
 }
