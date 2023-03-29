99ef991c0f1d8e7f82cea44fba9afc0cf1f46606
SLM <sabrina.martorelli@gmail.com>
2023-03-29 11:48:50 +0100

    Adds Browser Router

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,6 +3,7 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import BooksBoard from '../components/BooksBoard';
 
+
 const Home = () => {
     return (
         <>
